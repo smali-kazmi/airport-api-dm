2cf201a9ffc7ad785bef7c542e82fb0bbb8e71de
SM@K <smali.kazmi@hotmail.com>
2016-03-24 04:34:34 +0500

    complete 'GET /api/all/stats'

---
+++ test/controllers/api.js
@@ -0,0 +1,78 @@
+var fixtures = require('../fixtures');
+var should = require('should');
+var saveService = require('../../services/save');
+
+var _ = require('lodash');
+var async = require('async');
+
+var app = require('../../app');
+var request = require('supertest');
+
+describe('This section contains api service test cases', function() {
+  
+
+  require('../lib/testUtil').reset();
+
+  describe('GET /api/all/stats', function(){
+    var url = '/api/all/stats';
+    
+    it('should return emtpy list', function(done){
+      request(app)
+        .get(url)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) return done(err);
+          res.body.should.be.instanceof(Array);
+          res.body.length.should.equal(0)
+          done();
+        });
+    });
+
+    it('should return all records', function(done){
+
+      var RSVP = require('rsvp');
+      var records = fixtures.customData();
+      var promises = [];
+
+      async.eachSeries(records, function(record, cb){
+        saveService
+          .record(record)
+          .then(function(saved){
+            cb();
+          })
+          .catch(function(){
+            cb();
+          });
+      }, function(error){
+        request(app)
+          .get(url)
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .end(function(err, res) {
+            if (err) return done(err);
+
+            res.body.should.be.instanceof(Array);
+            res.body.length.should.equal(2);
+            res.body[0].should.have.property('id');
+            res.body[0].should.have.property('name');
+            res.body[0].should.have.property('total_reviews');
+            res.body[0].total_reviews.should.equal(3);
+
+            res.body[1].should.have.property('id');
+            res.body[1].should.have.property('name');
+            res.body[1].should.have.property('total_reviews');
+            res.body[1].total_reviews.should.equal(1);
+
+            done();
+          });
+          
+      });
+    });
+    
+    
+  });
+
+  
+
+});
