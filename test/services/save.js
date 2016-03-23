73caf6499922c31bd563c2060fc9b6ca3f13ae5b
SM@K <smali.kazmi@hotmail.com>
2016-03-23 21:16:38 +0500

    add test cases for save record

--- test/services/save.js
+++ test/services/save.js
@@ -2,6 +2,7 @@ var fixtures = require('../fixtures');
 var should = require('should');
 var saveService = require('../../services/save');
 var _ = require('lodash');
+
 describe('This section contains save service test cases', function() {
 
   describe('#record invalid data data set', function(){
@@ -72,5 +73,57 @@ describe('This section contains save service test cases', function() {
         });
     });  
   });
-  
+
+  describe('#record data set', function(){
+    beforeEach(function(done){
+      var RSVP = require('rsvp');
+      RSVP.all([db.sequelize.sync({force: true})]).then(function() {
+        done();
+      }).catch(function(reason){
+        done();
+      });
+    });
+
+    it('should save record', function(done){
+      var data = _.assignIn({}, 
+                  fixtures.Users.validData[0],
+                  fixtures.Airports.validData[0],
+                  fixtures.Reviews.validData[0]
+                );
+      saveService
+        .record(data)
+        .then(function(record){
+          record.should.not.equal({});
+          record.should.have.property('user');
+          record.should.have.property('airport');
+          record.should.have.property('review');
+          done();
+        })
+        .catch(function(error){
+          done(error);
+        });
+    });
+
+    it('should save record for unorder attributes', function(done){
+      var data = _.assignIn({}, 
+                  fixtures.Airports.validData[0],
+                  fixtures.Reviews.validData[0],
+                  fixtures.Users.validData[0]
+                );
+      saveService
+        .record(data)
+        .then(function(record){
+          record.should.not.equal({});
+          record.should.have.property('user');
+          record.should.have.property('airport');
+          record.should.have.property('review');
+          done();
+        })
+        .catch(function(error){
+          done(error);
+        });
+    });
+
+  });
+
 });
