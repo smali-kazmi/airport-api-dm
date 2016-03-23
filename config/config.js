cc41e623c35aee83e5756d6fa1cdf18173e4b7af
SM@K <smali.kazmi@hotmail.com>
2016-03-23 14:58:19 +0500

    add project config

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "aiport_api_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "logging": console.log,
+  },
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "aiport_api_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "logging": console.log,
+  }
+}
