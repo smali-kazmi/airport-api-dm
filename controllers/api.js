a110a3555ae17ea16d59c5868d4c164f6a520a06
SM@K <smali.kazmi@hotmail.com>
2016-03-24 05:56:09 +0500

    add apidoc details

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,18 @@
 
 var apiService = require('../services/api');
 
+/**
+ * @api {get} /api/all/stats Returns a collection of all airports stats
+ * @apiName Get all Stats
+ * @apiGroup Airport
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess (200) {Object[]} stats                  The collection airport review stats ordered by the count of reviews.
+ * @apiSuccess (200) {Number}   stats.id               id of the airport.
+ * @apiSuccess (200) {String}   stats.name             name of the airport.
+ * @apiSuccess (200) {Number}   stats.total_reviews    count of reviews in system.
+ * 
+ */
 
 exports.getAllStats = function(req, res) {
   
@@ -14,6 +26,23 @@ exports.getAllStats = function(req, res) {
     });
 };
 
+/**
+ * @api {get} /api/:airport_id/stats Returns an object of airport stats
+ * @apiName Get Airport Stats
+ * @apiGroup Airport
+ * @apiParam {Number} airport_id Airport unique ID.
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess (200) {Number}   id      id of the airport.
+ * @apiSuccess (200) {String}   name    name of the airport.
+ * @apiSuccess (200) {String}   link    link of the airport.
+ * @apiSuccess (200) {String}   title    title of the airport.
+ * @apiSuccess (200) {Number}   reviews    count of reviews.
+ * @apiSuccess (200) {Number}   average    average of "overall_rating".
+ * @apiSuccess (200) {Number}   recommended   count of recommendations "recommended"
+ * 
+ */
+
 exports.getAirportStats = function(req, res) {
 
   apiService
@@ -26,6 +55,23 @@ exports.getAirportStats = function(req, res) {
       res.status(500).json({'message': 'Internal Server Error'});
     });
 };
+
+/**
+ * @api {get} /api/:airport_id/reviews Returns an object of airport reviews
+ * @apiName Get List of all Airport's Review
+ * @apiGroup Airport
+ * @apiParam {Number} airport_id Airport unique ID.
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess (200) {Number}   id      id of the review.
+ * @apiSuccess (200) {Number}   overall_rating    overall rating of the review.
+ * @apiSuccess (200) {Number}   recommended    is recommended airport [1 = Yes | 0 = No].
+ * @apiSuccess (200) {Date}     date    date of review.
+ * @apiSuccess (200) {Object} author  Object of review author.
+ * @apiSuccess (200) {Number} author.id  id of the author.
+ * @apiSuccess (200) {String} author.name  name of the author.
+ * @apiSuccess (200) {String} author.country  country of the author.
+ */
 
 exports.getAirportReviews = function(req, res) {
 
